5a19d3724c53cdee07e24565f79d51f984cd52bd
dieuhan <dieuhan.mut@gmail.com>
2024-01-10 13:12:43 -0500

    wtf

---
+++ vite-project/js/array.js
@@ -0,0 +1,9 @@
+export const array = [
+    {
+        Rock: "rock",
+        Paper: "paper",
+        Scissors: "scissors",
+    }
+];
+
+console.log(array);
---
+++ vite-project/js/main.js
@@ -0,0 +1,3 @@
+import { DOMSelectors } from './dom'
+import './style.css'
+import { array} from './array'
